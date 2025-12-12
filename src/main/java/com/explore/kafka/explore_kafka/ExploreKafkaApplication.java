e2031c8b67faea225ad2e8b3db6befbb9db17e03
vasanthraj-qb <66236424+vasanthraj-qb@users.noreply.github.com>
2025-12-12 13:45:35 +0530

    Added Base Project Setup

---
+++ src/main/java/com/explore/kafka/explore_kafka/ExploreKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.explore.kafka.explore_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreKafkaApplication.class, args);
+	}
+
+}
